5fd5681656f9dffbb02f989a1a0ec8a77dfc8c6d
gm <gosha.gosha9696@gmail.com>
2023-01-07 21:23:51 +0300

    feat: create database connection

---
+++ server/src/helpers/connectToDb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export const connectToDb = async () => {
+  const dbUri = process.env.MONGODB_URI;
+
+  try {
+    if (!dbUri) {
+      throw new Error("Problem occured with environment variable MONGODB_URI!");
+    }
+
+    await mongoose.connect(dbUri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("DB connected");
+  } catch (error) {
+    console.error("Could not connect to db");
+    process.exit(1);
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,24 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import { connectToDb } from "./helpers/connectToDb";
 
 dotenv.config();
+const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(helmet());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Test connection");
 });
 
-const PORT = process.env.PORT;
+app.listen(PORT, async () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
 
-app.listen(PORT, () => {
-  console.log(`Server statred on port ${PORT}`);
+  await connectToDb();
 });
