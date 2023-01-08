da18cf772600b8824e5a47aa4d4c59aa6c671e8c
gm <gosha.gosha9696@gmail.com>
2023-01-08 18:15:10 +0300

    feat: create validate request middleware

---
+++ server/src/middlewares/validateRequest.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject } from "zod";
+import { StatusCodes } from "http-status-codes";
+
+export const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      return next();
+    } catch (error) {
+      return res.status(StatusCodes.BAD_REQUEST).json(error);
+    }
+  };
